6f58ece62b932263b3c3a04e7502d2dbadc19a38
Coder Tester <codewithme@outlook.com>
2018-06-03 17:38:50 +0530

    Updated to angular 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SideBarComponent } from './shared/sidebar/sidebar.component';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 
 
 @NgModule({
@@ -13,7 +18,12 @@ import { SideBarComponent } from './shared/sidebar/sidebar.component';
     SideBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase,'angular-auth-firebase'),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyBtV7Ks__2k3xTkx1lfbtdQId6XIr7REps",
+    authDomain: "fir-app-d26bd.firebaseapp.com",
+    databaseURL: "https://fir-app-d26bd.firebaseio.com",
+    projectId: "fir-app-d26bd",
+    storageBucket: "fir-app-d26bd.appspot.com",
+    messagingSenderId: "744243979761"
+  }
 };
